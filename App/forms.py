b7ac2cacc7f3a6f0d333e88bf40801ed3d49701d
Kashish <kashish042001@gmail.com>
2023-06-18 19:31:22 +0530

    forms create post

--- App/forms.py
+++ App/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import PostModel
 
 class PostModelForm(forms.ModelForm):
+    content = forms.CharField(widget=forms.Textarea({'rows':4}))
     class Meta:
         model = PostModel
         fields = ('title', 'content') 
--- Project/settings.py
+++ Project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'App',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -111,6 +112,8 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
 # Static files (CSS, JavaScript, Images)
