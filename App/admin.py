0517672c71ac5f62a8b04cb74666969b60dca74d
Kashish <kashish042001@gmail.com>
2023-06-18 19:06:04 +0530

    create postmodel

--- App/admin.py
+++ App/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post
+from .models import PostModel
+
+
+class PostModelAdmin(admin.ModelAdmin):
+  list_display = ('title',  'date_created')
+  search_fields = ['title', 'content']
+  
+admin.site.register(PostModel, PostModelAdmin)
--- App/models.py
+++ App/models.py
@@ -2,20 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
-STATUS = ((0,"Draft"), (1,"Published"))
+STATUS = ((0, "Draft"), (1, "Published"))
 
-class Post(models.Model):
-    title: models.CharField(max_length=200, unique=True)
-    slug= models.SlugField(max_length=200, unique=True)
-    author: models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
-    created_on= models.DateTimeField(auto_now_add=True)
-    updated_on= models.DateTimeField(auto_now=True)
-    content= models.TextField()
-    status= models.IntegerField(choices=STATUS, default=0)
+class PostModel(models.Model):
+  title =  models.CharField(max_length=100)
+  author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+  date_created = models.DateTimeField(auto_now_add=True)
+  content = models.TextField()
+  
 
-    class Meta:
-        ordering =['-created_on']
+  class Meta:
+    ordering = ['date_created']
 
-    def __str__(self):
-        return self.title
-    
+  def __str__(self):
+    return self.title
